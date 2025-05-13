fc302572f562dfbd23ae5b714190fbaa4224ab60
Gemdave <Geralde.jnr@gmail.com>
2025-05-13 10:52:02 +0200

    Sticky nav

---
+++ Public/Scripts/script.js
@@ -0,0 +1,11 @@
+window.addEventListener("scroll", function () {
+    var navbar = document.getElementById("navbar");
+    var header = document.querySelector(".header-body");
+    var headerBottom = header.offsetTop + header.offsetHeight - navbar.offsetHeight*2;
+
+    if (window.scrollY >= headerBottom) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+});
