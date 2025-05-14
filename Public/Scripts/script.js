47deaab6e14acfe94c814aebc912a662fb4c7c42
Ronin326 <ruanleroux37@gmail.com>
2025-05-14 22:14:42 +0200

    Footer Complete

--- Public/Scripts/script.js
+++ Public/Scripts/script.js
@@ -2,7 +2,7 @@
 window.addEventListener("scroll", function () {
     var navbar = document.getElementById("navbar");
     var header = document.querySelector(".header-body");
-    var headerBottom = header.offsetTop + header.offsetHeight - navbar.offsetHeight*2;
+    var headerBottom = header.offsetTop + header.offsetHeight - navbar.offsetHeight*1;
 
     if (window.scrollY >= headerBottom) {
         navbar.classList.add("sticky");
